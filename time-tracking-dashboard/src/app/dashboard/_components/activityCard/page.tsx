86a1d934c2e3339c7eb1575ff7f1f46300dd13f1
konraddissake1808 <konrad.dissakengando@gmail.com>
2024-11-28 13:59:16 +0100

    Created the cards

--- time-tracking-dashboard/src/app/dashboard/_components/activityCard/page.tsx
+++ time-tracking-dashboard/src/app/dashboard/_components/activityCard/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Image from 'next/image'
 
-function Activity() {
+interface ActivityProps{
+  color:string;
+}
+
+export const Activity: React.FC<ActivityProps> = ({color}) => {
   return (
     <div className='w-[90%]'>
         <div className='w-full h-32 relative'>
-            <div className='w-full h-1/2 z-10 absolute'></div>
-            <div className='bg-darkBlue w-full h-3/4 z-0 absolute bottom-0'></div>
+            <div style={{backgroundColor:color}} className='w-full h-1/2 z-0 absolute'></div>
+            <div className='bg-darkBlue w-full h-3/4 z-10 absolute bottom-0'></div>
         </div>
     </div>
   )
--- time-tracking-dashboard/src/app/dashboard/page.tsx
+++ time-tracking-dashboard/src/app/dashboard/page.tsx
@@ -6,7 +6,24 @@ function Dashboard() {
   return (
     <div className='flex justify-center pt-16 flex-col items-center'>
         <User></User>
-        <Activity></Activity>
+        <div className='w-full flex justify-center'>
+          <Activity color='hsl(15, 100%, 70%)'></Activity>
+        </div>
+        <div className='w-full flex justify-center'>
+          <Activity color='hsl(195, 74%, 62%)'></Activity>
+        </div>
+        <div className='w-full flex justify-center'>
+          <Activity color='hsl(348, 100%, 68%)'></Activity>
+        </div>
+        <div className='w-full flex justify-center'>
+          <Activity color='hsl(145, 58%, 55%)'></Activity>
+        </div>
+        <div className='w-full flex justify-center'>
+          <Activity color='hsl(264, 64%, 52%)'></Activity>
+        </div>
+        <div className='w-full flex justify-center'>
+          <Activity color='hsl(43, 84%, 65%)'></Activity>
+        </div>
     </div>
   )
 }
