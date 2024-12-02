84ff93b24fe623fc598dac0155efd89d1e9c4a75
konraddissake1808 <konrad.dissakengando@gmail.com>
2024-12-02 13:22:50 +0100

    Implemented the src prop for Images in the activity card

--- time-tracking-dashboard/src/app/dashboard/_components/activityCard/page.tsx
+++ time-tracking-dashboard/src/app/dashboard/_components/activityCard/page.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 
 interface ActivityProps{
   color:string;
+  imageSrc:StaticImageData;
 }
 
-export const Activity: React.FC<ActivityProps> = ({color}) => {
+export const Activity: React.FC<ActivityProps> = ({color, imageSrc}) => {
   return (
     <div className='w-[90%]'>
         <div className='w-full h-32 relative'>
-            <div style={{backgroundColor:color}} className='w-full h-1/2 z-0 absolute'></div>
+            <div style={{backgroundColor:color}} className='w-full h-1/2 z-0 absolute'>
+              <div>
+                <Image src={imageSrc} alt=''></Image>
+              </div>
+            </div>
             <div className='bg-darkBlue w-full h-3/4 z-10 absolute bottom-0'></div>
         </div>
     </div>
--- time-tracking-dashboard/src/app/dashboard/page.tsx
+++ time-tracking-dashboard/src/app/dashboard/page.tsx
@@ -1,28 +1,34 @@
 import React from 'react'
 import User from './_components/user/page'
 import Activity from './_components/activityCard/page'
+import briefCase from './../../../public/images/icon-work.svg'
+import  controller from './../../../public/images/icon-play.svg'
+import book from './../../../public/images/icon-study.svg'
+import exercise from './../../../public/images/icon-exercise.svg'
+import discussionBubble from './../../../public/images/icon-social.svg'
+import heart from './../../../public/images/icon-self-care.svg'
 
 function Dashboard() {
   return (
     <div className='flex justify-center pt-16 flex-col items-center'>
         <User></User>
         <div className='w-full flex justify-center'>
-          <Activity color='hsl(15, 100%, 70%)'></Activity>
+          <Activity imageSrc={briefCase} color='hsl(15, 100%, 70%)'></Activity>
         </div>
         <div className='w-full flex justify-center'>
-          <Activity color='hsl(195, 74%, 62%)'></Activity>
+          <Activity imageSrc={controller} color='hsl(195, 74%, 62%)'></Activity>
         </div>
         <div className='w-full flex justify-center'>
-          <Activity color='hsl(348, 100%, 68%)'></Activity>
+          <Activity imageSrc={book} color='hsl(348, 100%, 68%)'></Activity>
         </div>
         <div className='w-full flex justify-center'>
-          <Activity color='hsl(145, 58%, 55%)'></Activity>
+          <Activity imageSrc={exercise} color='hsl(145, 58%, 55%)'></Activity>
         </div>
         <div className='w-full flex justify-center'>
-          <Activity color='hsl(264, 64%, 52%)'></Activity>
+          <Activity imageSrc={discussionBubble} color='hsl(264, 64%, 52%)'></Activity>
         </div>
         <div className='w-full flex justify-center'>
-          <Activity color='hsl(43, 84%, 65%)'></Activity>
+          <Activity imageSrc={heart} color='hsl(43, 84%, 65%)'></Activity>
         </div>
     </div>
   )
