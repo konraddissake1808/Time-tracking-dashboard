edfaa99b4a296a57d261a4400a73bb8e1c5a178a
konraddissake1808 <konrad.dissakengando@gmail.com>
2024-12-31 07:26:34 +0100

    Set up the the data update on the ui on timeframe button click

--- time-tracking-dashboard/src/app/dashboard/_components/user/page.tsx
+++ time-tracking-dashboard/src/app/dashboard/_components/user/page.tsx
@@ -5,12 +5,14 @@ import UserImage from '../../../../../public/images/image-jeremy.png'
 
 //add a function for each button to determine the time frame to display and export it to the activity card component using use state
 type userProp = {
-  dailyData: number;
-  sendToDashboard: any;
+  workDailyData: number;
+  dailyDataForDashboard: any;
+  weeklyDataForDashboard: any;
+  monthlyDataFromDashboard: any;
 };
 
 
-const User: React.FC<userProp> = ({data, sendToDashboard}) => {
+const User: React.FC<userProp> = ({data, dailyDataForDashboard, weeklyDataForDashboard, monthlyDataFromDashboard}) => {
 
   function dailyTimeframe() {
     const dailyButton = document.getElementById('daily')
@@ -19,8 +21,15 @@ const User: React.FC<userProp> = ({data, sendToDashboard}) => {
     weeklyButton?.classList.remove('active');
     const monthlyButton = document.getElementById('monthly');
     monthlyButton?.classList.remove('active');
-    const dailyData = data[0].timeframes.daily.current;
-    sendToDashboard(dailyData);
+    const dailyData = [
+      data[0].timeframes.daily.current,
+      data[1].timeframes.daily.current,
+      data[2].timeframes.daily.current,
+      data[3].timeframes.daily.current,
+      data[4].timeframes.daily.current,
+      data[5].timeframes.daily.current,
+    ];
+    dailyDataForDashboard(dailyData);
   }
  
 
@@ -31,6 +40,15 @@ const User: React.FC<userProp> = ({data, sendToDashboard}) => {
     monthlyButton?.classList.remove('active');
     const dailyButton = document.getElementById('daily')
     dailyButton?.classList.remove('active');
+    const weeklyData = [
+      data[0].timeframes.weekly.current,
+      data[1].timeframes.weekly.current,
+      data[2].timeframes.weekly.current,
+      data[3].timeframes.weekly.current,
+      data[4].timeframes.weekly.current,
+      data[5].timeframes.weekly.current,
+    ];
+    weeklyDataForDashboard(weeklyData)
   }
 
   function monthlyTimeframe() {
@@ -40,7 +58,17 @@ const User: React.FC<userProp> = ({data, sendToDashboard}) => {
     dailyButton?.classList.remove('active');
     const weeklyButton = document.getElementById('weekly');
     weeklyButton?.classList.remove('active');
+    const monthlyData = [
+      data[0].timeframes.monthly.current,
+      data[1].timeframes.monthly.current,
+      data[2].timeframes.monthly.current,
+      data[3].timeframes.monthly.current,
+      data[4].timeframes.monthly.current,
+      data[5].timeframes.monthly.current,
+    ];
+    monthlyDataFromDashboard(monthlyData)
   }
+
 
    // const [active,setActive] = useState(dailyTimeframe)
 
--- time-tracking-dashboard/src/app/dashboard/page.tsx
+++ time-tracking-dashboard/src/app/dashboard/page.tsx
@@ -12,12 +12,31 @@ import heart from './../../../public/images/icon-self-care.svg'
 
 function Dashboard({data}) {
 
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState(
+    [
+      data[0].timeframes.daily.current, 
+      data[1].timeframes.daily.current, 
+      data[2].timeframes.daily.current, 
+      data[3].timeframes.daily.current, 
+      data[4].timeframes.daily.current, 
+      data[5].timeframes.daily.current
+    ]
+  );
   //console.log(data)
-  const dailyTimeframe = (dailyButtonData: number) => {
+  const dailyTimeframe = (dailyButtonData: array) => {
     setTime(dailyButtonData);
     console.log(dailyButtonData);
   }
+
+  const weeklyTimeframe = (weeklyButtonData: array) => {
+    setTime(weeklyButtonData);
+    console.log(weeklyButtonData);
+  };
+
+  const monthlyTimeframe = (monthlyButtonData: array) => {
+    setTime(monthlyButtonData);
+    console.log(monthlyButtonData);
+  };
 
 
   //const work = data[0];
@@ -27,24 +46,24 @@ function Dashboard({data}) {
 
   return (
     <div className='flex justify-center pt-16 flex-col items-center'>
-      <User sendToDashboard={dailyTimeframe} data={data}></User>
+      <User monthlyDataFromDashboard={monthlyTimeframe} weeklyDataForDashboard={weeklyTimeframe} dailyDataForDashboard={dailyTimeframe} data={data}></User>
       <div className='w-full flex justify-center'>
-        <Activity time={time} title={data[0].title} imageSrc={briefCase} color='hsl(15, 100%, 70%)'></Activity>
+        <Activity time={time[0]} title={data[0].title} imageSrc={briefCase} color='hsl(15, 100%, 70%)'></Activity>
       </div>
       <div className='w-full flex justify-center'>
-        <Activity title={data[1].title} imageSrc={controller} color='hsl(195, 74%, 62%)'></Activity>
+        <Activity time={time[1]} title={data[1].title} imageSrc={controller} color='hsl(195, 74%, 62%)'></Activity>
       </div>
       <div className='w-full flex justify-center'>
-        <Activity title={data[2].title} imageSrc={book} color='hsl(348, 100%, 68%)'></Activity>
+        <Activity time={time[2]} title={data[2].title} imageSrc={book} color='hsl(348, 100%, 68%)'></Activity>
       </div>
       <div className='w-full flex justify-center'>
-        <Activity title={data[3].title} imageSrc={exercise} color='hsl(145, 58%, 55%)'></Activity>
+        <Activity time={time[3]} title={data[3].title} imageSrc={exercise} color='hsl(145, 58%, 55%)'></Activity>
       </div>
       <div className='w-full flex justify-center'>
-        <Activity title={data[4].title} imageSrc={discussionBubble} color='hsl(264, 64%, 52%)'></Activity>
+        <Activity time={time[4]} title={data[4].title} imageSrc={discussionBubble} color='hsl(264, 64%, 52%)'></Activity>
       </div>
       <div className='w-full flex justify-center'>
-        <Activity title={data[5].title} imageSrc={heart} color='hsl(43, 84%, 65%)'></Activity>
+        <Activity time={time[5]} title={data[5].title} imageSrc={heart} color='hsl(43, 84%, 65%)'></Activity>
       </div>
     </div>
   )
