55a76c2bf5ac7c1fd91f182232b0e4b48587432c
konraddissake1808 <konrad.dissakengando@gmail.com>
2025-01-02 23:49:29 +0100

    Added a conditional statement to check data before passing it as a prop

--- time-tracking-dashboard/src/app/page.tsx
+++ time-tracking-dashboard/src/app/page.tsx
@@ -5,6 +5,12 @@ import { promises as fs } from 'fs'
 export default async function Home() {
   const file = await fs.readFile(process.cwd() + '/data/data.json', 'utf8');
   const data = JSON.parse(file);
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
   return (
     <div className="bg-veryDarkBlue font-rubik xl:h-screen">
       <div className="xl:w-full xl:h-full xl:flex xl:justify-center xl:items-center">
